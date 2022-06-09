98a033bf991836f3ce9237ec3700861300a2e36b
Jaydeep <jaydeep.dey03@gmail.com>
2022-06-09 11:51:58 +0530

    added LCS

--- dynamic programming/LCSbased/LCS.cpp
+++ dynamic programming/LCSbased/LCS.cpp
@@ -1,37 +1,63 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
+// longest common subsqueuence Bottom-up
 
- // } Driver Code Ends
-class Solution{
-    public:
+class Solution {
+public:
+    int LCS(string x, string y, int n, int m){
+        
+        int dp[1001][1001];
+        
+        memset(dp, -1, sizeof(dp));
+        
+        if(dp[n][m] != -1)return dp[n][m];
+        
+        if(n == 0 || m == 0) return 0;
+        
+        if(x[n-1] == y[m-1]){
+            return dp[n][m] =  1 + LCS(x, y, n-1, m-1);
+        }
+        
+        else {
+            return dp[n][m] = max(LCS(x, y, n-1, m), LCS(x, y, n, m-1));
+        }
+    }
     
-    int longestCommonSubstr (string x, string y, int n, int m)
-    {
-       if(n==0 || m == 0)return 0;
-       if(x[n-1] == y[m-1]){
-           return 1+longestCommonSubstr(x, y, n-1, m-1);
-       }
-       else{
-           return max(longestCommonSubstr(x, y, n-1, m), longestCommonSubstr(x, y, n, m-1));
-       }
+    int longestCommonSubsequence(string x, string y) {
+        int n = x.length();
+        int m = y.length();
+        
+        return LCS(x, y, n, m);
     }
 };
 
-// { Driver Code Starts.
+// LCS top-down
 
-int main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        int n, m; cin >> n >> m;
-        string s1, s2;
-        cin >> s1 >> s2;
-        Solution ob;
-
-        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+class Solution {
+public:    
+    int longestCommonSubsequence(string x, string y) {
+         // initialization
+        
+        int n = x.length();
+        int m = y.length();
+        int dp[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (i == 0)dp[i][j] = 0;
+                if (j == 0)dp[i][j] = 0;
+            }
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(x[i-1] == y[j-1]){
+                    dp[i][j] =  1 + dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
-}
-// Contributed By: Pranay Bansal
-  // } Driver Code Ends
+};
